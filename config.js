07082147df52ad0e946a3fbaae2bb9760afea14d
Rodrigo Cezario <contato.rodrigoc@gmail.com>
2020-06-23 09:10:16 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -7,12 +7,124 @@ const ENFERMEIROS = [
 /*
                Id:  Identificador do funcionário (Somente no sistema)
              Nome:  Nome do usuário
-          Funcoes:  Preferência de função [0: Nunca, 1:Mínimo, 2:Normal, 3:Máxima]
+          Funcoes:  Preferência de função [0: Nunca, 1:Mínimo, 2/Vazio:Normal, 3:Máxima]
         Afinidade:  Positiva/Negativa com algum funcionário [Colocar o Id do funcionário]
 */
     {
         "Id": 1,
-        "Nome": "CAMILA PAUL",
+        "Nome": "NOME EXEMPLO 1",
+        "Funcoes": [
+            { "PP": 1 },
+            { "AC": 3 },
+            { "P": 2 },
+            { "OBS": 0 },
+        ],
+        "Afinidade": [
+            { "Positiva": [2] },
+            { "Negativa": [3] },
+        ]
+    },
+    {
+        "Id": 2,
+        "Nome": "NOME EXEMPLO 2",
+        "Funcoes": [
+            { "PP": 1 },
+            { "AC": 3 },
+            { "P": 2 },
+            { "OBS": 0 },
+        ],
+        "Afinidade": [
+            { "Positiva": [2] },
+            { "Negativa": [3] },
+        ]
+    },
+    {
+        "Id": 3,
+        "Nome": "NOME EXEMPLO 3",
+        "Funcoes": [
+            { "PP": 1 },
+            { "AC": 3 },
+            { "P": 2 },
+            { "OBS": 0 },
+        ],
+        "Afinidade": [
+            { "Positiva": [2] },
+            { "Negativa": [3] },
+        ]
+    },
+    {
+        "Id": 4,
+        "Nome": "NOME EXEMPLO 4",
+        "Funcoes": [
+            { "PP": 1 },
+            { "AC": 3 },
+            { "P": 2 },
+            { "OBS": 0 },
+        ],
+        "Afinidade": [
+            { "Positiva": [2] },
+            { "Negativa": [3] },
+        ]
+    },
+    {
+        "Id": 5,
+        "Nome": "NOME EXEMPLO 5",
+        "Funcoes": [
+            { "PP": 1 },
+            { "AC": 3 },
+            { "P": 2 },
+            { "OBS": 0 },
+        ],
+        "Afinidade": [
+            { "Positiva": [2] },
+            { "Negativa": [3] },
+        ]
+    },
+    {
+        "Id": 6,
+        "Nome": "NOME EXEMPLO 6",
+        "Funcoes": [
+            { "PP": 1 },
+            { "AC": 3 },
+            { "P": 2 },
+            { "OBS": 0 },
+        ],
+        "Afinidade": [
+            { "Positiva": [2] },
+            { "Negativa": [3] },
+        ]
+    },
+    {
+        "Id": 7,
+        "Nome": "NOME EXEMPLO 7",
+        "Funcoes": [
+            { "PP": 1 },
+            { "AC": 3 },
+            { "P": 2 },
+            { "OBS": 0 },
+        ],
+        "Afinidade": [
+            { "Positiva": [2] },
+            { "Negativa": [3] },
+        ]
+    },
+    {
+        "Id": 8,
+        "Nome": "NOME EXEMPLO 8",
+        "Funcoes": [
+            { "PP": 1 },
+            { "AC": 3 },
+            { "P": 2 },
+            { "OBS": 0 },
+        ],
+        "Afinidade": [
+            { "Positiva": [2] },
+            { "Negativa": [3] },
+        ]
+    },
+    {
+        "Id": 9,
+        "Nome": "NOME EXEMPLO 9",
         "Funcoes": [
             { "PP": 1 },
             { "AC": 3 },
@@ -39,46 +151,46 @@ const FUNCOES = [
             "Id": 1,
             "AlocacaoDiaria": 1,
             "AdicionalPrioridade": 1,
-            "Ligacao": [4]
+            "Ligacao": [3,4]
         }
     },
     {
         "PP": {
             "Id": 2,
             "AlocacaoDiaria": 1,
-            "AdicionalPrioridade": 1,
-            "Ligacao": [2, 3]
+            "AdicionalPrioridade": 3,
+            "Ligacao": [3,4]
         }
     },
     {
         "P": {
             "Id": 3,
-            "AlocacaoDiaria": 1,
-            "AdicionalPrioridade": 1,
-            "Ligacao": [2, 3]
+            "AlocacaoDiaria": 2,
+            "AdicionalPrioridade": 3,
+            "Ligacao": [1, 2]
         }
     },
     {
         "RN": {
             "Id": 4,
             "AlocacaoDiaria": 1,
-            "AdicionalPrioridade": 1,
-            "Ligacao": [1]
+            "AdicionalPrioridade": 2,
+            "Ligacao": [1,2]
         }
     },
     {
         "PATO": {
             "Id": 5,
             "AlocacaoDiaria": 1,
-            "AdicionalPrioridade": 1,
+            "AdicionalPrioridade": 3,
             "Ligacao": [2, 3]
         }
     },
     {
-        "M/C": {
+        "MC": {
             "Id": 6,
-            "AlocacaoDiaria": 1,
-            "AdicionalPrioridade": 1,
+            "AlocacaoDiaria": 2,
+            "AdicionalPrioridade": 3,
             "Ligacao": [2, 3]
         }
     },
@@ -86,7 +198,7 @@ const FUNCOES = [
         "OBS": {
             "Id": 7,
             "AlocacaoDiaria": 1,
-            "AdicionalPrioridade": 1,
+            "AdicionalPrioridade": 3,
             "Ligacao": [2, 3]
         }
     },
@@ -94,7 +206,7 @@ const FUNCOES = [
         "CTB": {
             "Id": 8,
             "AlocacaoDiaria": 1,
-            "AdicionalPrioridade": 1,
+            "AdicionalPrioridade": 3,
             "Ligacao": [2, 3]
         }
     },
@@ -102,7 +214,7 @@ const FUNCOES = [
         "AC": {
             "Id": 9,
             "AlocacaoDiaria": 1,
-            "AdicionalPrioridade": 1,
+            "AdicionalPrioridade": 3,
             "Ligacao": [2, 3]
         }
     },
@@ -127,7 +239,7 @@ const FUNCOES = [
 
 
 const SELECTMESDATA = {
-    "0": "Selecione",
+    "0": "Selecione...",
     "1": "Janeiro",
     "2": "Fevereiro",
     "3": "Março",
--- content.js
+++ content.js
@@ -1,4 +1,6 @@
 var listaDias = [];
+var listaEnf = [];
+var listaFuncoes = [];
 
 window.escalaUi = {
     init: function () {
@@ -18,14 +20,62 @@ window.escalaUi = {
 
     popularTableDias: function () {
 
-        $("#tableData").empty();
-        $("#tableData").append('<tr><th scope="col">Data: </th></tr>');
-
         listaDias = window.escalaFunctions.getListaDias();
 
         for (var i = 0; i < listaDias.length; i++) {
-            $("#tableData > tr").append('<th scope="col"> ' + listaDias[i] + ' </th>');
+            $("#tableDataEnf > tr").append('<th> ' + listaDias[i] + ' </th>');
         }
+    },
+
+    popularTableEnf: function () {
+
+        listaEnf = window.escalaFunctions.getListaEnf();
+
+        for (var x = 0; x < listaEnf.length; x++) {
+            $("#tableEnf").append('<tr id="tr' + listaEnf[x].Id + '"></tr>');
+            $("#tr" + listaEnf[x].Id).append('<th scope="row" colspan="3">' + (x + 1) + ' - ' + listaEnf[x].Nome + ' </th>');
+        }
+
+        if (listaEnf.length > 0) {
+            $("#btnGerarFuncoes").removeAttr('disabled')
+        }
+    },
+
+    popularTableBranco: function () {
+
+        for (var x = 0; x < listaDias.length; x++) {
+            for (var y = 0; y < listaEnf.length; y++) {
+                $("#tr" + listaEnf[y].Id).append('<td> <select class="form-control-sm" id="exampleFormControlSelect1">' +
+                    '<option></option>' +
+                    '<option>PAT</option>' +
+                    '</select > </td > ');
+            }
+        }
+    },
+
+    popularFuncoes: function () {
+        $("#tableEnf").empty();
+        window.escalaUi.popularTableEnf();
+
+        listaFuncoes = window.escalaFunctions.getListaFuncoes();
+
+        for (var x = 0; x < listaDias.length; x++) {
+            var sequenciaDia = window.escalaFunctions.getSequenciaEnfDia(9);
+
+            for (var y = 0; y < listaEnf.length; y++) {
+                $("#tr" + listaEnf[y].Id).append('<td>' + sequenciaDia[y] + ' </td>');
+            }
+        }
+    },
+
+    limparDadosTabela: function () {
+
+        $("#tableDataEnf").empty();
+        $("#tableDataEnf").append('<tr><th scope="col" colspan="3">Data: </th></tr>');
+
+        $("#tableEnf").empty();
+
+        $("#btnGerarFuncoes").attr('disabled', 'disabled')
     }
 };
 
@@ -49,6 +99,39 @@ window.escalaFunctions = {
         }
 
         return dias;
+    },
+
+    getListaEnf: function () {
+        return ENFERMEIROS;
+    },
+
+    getListaFuncoes: function () {
+        return FUNCOES;
+    },
+
+    getSequenciaEnfDia: function (enfNoDia) {
+        let funcoes = "SL,PP,P,P,RN,PATO,MC,MC,OBS,CTB,AC".split(",");
+        var funcoesNoDia = [];
+
+        let numerosSorteados = [];
+
+        let geradorNumero = function (lista) {
+            if (lista.length >= enfNoDia)
+                return;
+            let novoNumero = Math.floor(Math.random() * enfNoDia + 1);
+            if (lista.indexOf(novoNumero) < 0) {
+                lista.push(novoNumero);
+            }
+            geradorNumero(lista);
+        };
+
+        geradorNumero(numerosSorteados);
+
+        for (var i = 0; i < enfNoDia; i++) {
+            funcoesNoDia.push(funcoes[numerosSorteados[i]]);
+        }
+
+        return funcoesNoDia;
     }
 };
 
@@ -57,14 +140,26 @@ window.escalaEvents = {
 
     },
 
-    selectMesOnChange: function () {
-        if ($("#selectMes").val() != "0")
+    onChangeSelectMes: function () {
+        window.escalaUi.limparDadosTabela();
+
+        if ($("#selectMes").val() != "0") {
             window.escalaUi.popularTableDias();
+            window.escalaUi.popularTableEnf();
+            window.escalaUi.popularTableBranco();
+        }
     },
 
-    selectParImparOnChange: function () {
-        if ($("#selectMes").val() != "0")
-            window.escalaUi.popularTableDias();
+    onChangeSelectParImpar: function () {
+        if ($("#selectMes").val() != "0") {
+            window.escalaUi.limparDadosTabela();
+            window.escalaUi.popularTableEnf();
+            window.escalaUi.popularTableBranco();
+        }
+    },
+
+    onClickGerarFuncoes: function () {
+        window.escalaUi.popularFuncoes();
     }
 };
 
